66cae405c3ecbac095d6a74cfee61dc468738efd
martinimatteo10 <matteo.martini02@universitadipavia.it>
2022-04-26 22:33:47 +0200

    Aggiunta classe TicketBuilder.java e Test.java

---
+++ src/main/java/Test.java
@@ -0,0 +1,42 @@
+import java.io.File;
+
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+        File source = new File(TicketBuilder.DEST);
+        String path = "src/main/resources/results/";
+
+        // System.currentTimeMillis usato per avere nomi tutti diversi
+        String idTicket = "CS" + System.currentTimeMillis();
+        String idVoucher = "VO" + System.currentTimeMillis();
+        String idVoucherRegalo = "VOR" + System.currentTimeMillis();
+
+        TicketBuilder ticket = new TicketBuilder("Milano", "Roma", "2022-10-10", "2022-10-10", "07:00", "19:00", "Mario", "Rossi", "1/1/2000",idTicket,"50€","1","0","0","0");
+        TicketBuilder voucher = new TicketBuilder(idVoucher,"100€");
+        TicketBuilder voucherRegalo = new TicketBuilder(idVoucherRegalo,"200€","Tanti Auguri!");
+
+        //creazione dei file pdf, visualizzavili in cartella "results"
+        ticket.createPdf();
+            TicketBuilder.copy(source, new File(path.concat(idTicket).concat(".pdf")));
+        voucher.createPdf();
+            TicketBuilder.copy(source, new File(path.concat(idVoucher).concat(".pdf")));
+        voucherRegalo.createPdf();
+            TicketBuilder.copy(source, new File(path.concat(idVoucherRegalo).concat(".pdf")));
+
+
+            //Thread che dopo 5 secondi, cancella il file nella cartella temporanea
+            Thread t = new Thread(() -> {
+                try {
+                    Thread.sleep(6000);
+                    source.delete();
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            t.start();
+
+            //Non conosci la tua cartella temporanea? allora esegui questo comando
+            //System.out.println(System.getProperty("java.io.tmpdir"));
+    }
+}
---
+++ src/main/java/TicketBuilder.java
@@ -0,0 +1,221 @@
+import com.itextpdf.text.pdf.AcroFields;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ *
+ * Classe che si occupa di creare e riempire i biglietti in pdf
+ * See <a href="https://api.itextpdf.com/iText5/java/5.5.13.3/">IText 5.5.13.3</a>
+ *
+ * @author martinimatteo10
+ */
+public class TicketBuilder {
+
+    /**
+     * DEST serve per ottenere il percorso delle cartelle di sistema che contengono file temporanei.
+     */
+    public static final String DEST = System.getProperty("java.io.tmpdir").concat("NuovoBiglietto.pdf");
+    /**
+     * SRC contiene il percorso del template per il biglietto corsa singola.
+     */
+    public static final String SRC = "src/main/resources/assets/TemplateTicketIcon.pdf";
+    /**
+     * SRCVO contiene il percorso del template per il voucher (generato dopo un rimborso).
+     */
+    public static final String SRCVO = "src/main/resources/assets/TemplateVoucher.pdf";
+    /**
+     * SRCVOR contiene il percorso del template per il voucher regalo (o gift card) acquistabile dalla sezione apposita.
+     */
+    public static final String SRCVOR = "src/main/resources/assets/TemplateGiftCard.pdf";
+
+    private String sPart;
+    private String sDest;
+    private String dataPart;
+    private String dataArr;
+    private String oraPart;
+    private String oraArr;
+    private String nome;
+    private String cognome;
+    private String dataNascita;
+    private final String id;
+    private final String importo;
+    private String messaggio;
+    private String adulti;
+    private String ragazzi;
+    private String bambini;
+    private String animali;
+
+
+    /**
+     * Costruttore richiamato nel caso in cui si voglia generare un biglietto corsa singola.
+     *
+     * @param sPart
+     * @param sDest
+     * @param dataPart
+     * @param dataArr
+     * @param oraPart
+     * @param oraArr
+     * @param nome
+     * @param cognome
+     * @param dataNascita
+     * @param id
+     * @param importo
+     * @param adulti
+     * @param ragazzi
+     * @param bambini
+     * @param animali
+     */
+    public TicketBuilder(String sPart, String sDest, String dataPart, String dataArr, String oraPart, String oraArr,
+                         String nome, String cognome, String dataNascita, String id, String importo, String adulti, String ragazzi, String bambini, String animali) {
+
+        this.sPart = sPart;
+        this.sDest = sDest;
+        this.dataPart = dataPart;
+        this.dataArr = dataArr;
+        this.oraPart = oraPart;
+        this.oraArr = oraArr;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.id = id;
+        this.importo = importo;
+        this.adulti = adulti;
+        this.ragazzi = ragazzi;
+        this.bambini = bambini;
+        this.animali = animali;
+    }
+
+    /**
+     * Costruttore richiamato nel caso in cui si voglia generare un voucher.
+     *
+     * @param id
+     * @param importo
+     */
+    public TicketBuilder(String id, String importo) {
+        this.importo = importo;
+        this.id = id;
+    }
+
+    /**
+     * Costruttore richiamato nel caso in cui si voglia generare un voucher regalo (o Gift Card).
+     *
+     * @param id
+     * @param importo
+     * @param messaggio
+     */
+    public TicketBuilder(String id, String importo, String messaggio) {
+        this.importo = importo;
+        this.id = id;
+        this.messaggio = messaggio;
+    }
+
+    /**
+     * Metodo che, quando richiamato, si occupa di copiare un file esistente (source) in un nuovo file (target).
+     * (Non è consentito sovrascrivere un file con lo stesso nome).
+     *
+     * @param source
+     * @param target
+     * @throws IOException
+     */
+    public static void copy(File source, File target) throws IOException {
+        FileChannel sourceChannel = new FileInputStream(source).getChannel();
+        FileChannel targetChannel = new FileOutputStream(target).getChannel();
+
+        targetChannel.transferFrom(sourceChannel, 0,
+                sourceChannel.size());
+
+        targetChannel.close();
+        sourceChannel.close();
+    }
+
+    /**
+     * Metodo generale che, ricevuto un id di un biglietto, ne analizza i primi 2 caratteri per restituire la funzione
+     * di creazione del pdf.
+     *
+     * @throws Exception
+     */
+    public void createPdf() throws Exception {
+        if (this.id.startsWith("CS"))
+            createCS();
+        if (this.id.startsWith("VO"))
+            createVO();
+        else if (this.id.startsWith("VOR"))
+            createVORegalo();
+    }
+
+    /**
+     * Metodo che crea (e compila) il pdf del biglietto corsa singola.
+     *
+     * @throws Exception
+     */
+    private void createCS() throws Exception {
+        PdfReader originale = new PdfReader(SRC);
+        PdfStamper copiaModificata = new PdfStamper(originale, new FileOutputStream(DEST));
+        AcroFields placeholder = copiaModificata.getAcroFields();
+
+        placeholder.setField("STAZIONEPARTENZA", this.sPart);
+        placeholder.setField("STAZIONEARRIVO", this.sDest);
+        placeholder.setField("DATAPARTENZA", this.dataPart);
+        placeholder.setField("DATAARRIVO", this.dataArr);
+        placeholder.setField("ORARIOPARTENZA", this.oraPart);
+        placeholder.setField("ORARIOARRIVO", this.oraArr);
+        placeholder.setField("NOME", this.nome);
+        placeholder.setField("COGNOME", this.cognome);
+        placeholder.setField("DATANASCITA", this.dataNascita);
+        placeholder.setField("BIGLIETTO", this.id);
+        placeholder.setField("EURO", this.importo);
+
+        placeholder.setField("AD", this.adulti);
+        placeholder.setField("R", this.ragazzi);
+        placeholder.setField("B", this.bambini);
+        placeholder.setField("AN", this.animali);
+
+        copiaModificata.setFormFlattening(true);
+        copiaModificata.close();
+        originale.close();
+    }
+
+    /**
+     * Metodo che crea (e compila) il pdf del voucher.
+     *
+     * @throws Exception
+     */
+    private void createVO() throws Exception {
+        PdfReader originale = new PdfReader(SRCVO);
+        PdfStamper copiaModificata = new PdfStamper(originale, new FileOutputStream(DEST));
+        AcroFields placeholder = copiaModificata.getAcroFields();
+
+        placeholder.setField("ID", this.id);
+        placeholder.setField("EURO", this.importo);
+
+        copiaModificata.setFormFlattening(true);
+        copiaModificata.close();
+        originale.close();
+    }
+
+    /**
+     * Metodo che crea (e compila) il pdf della gift card.
+     *
+     * @throws Exception
+     */
+    private void createVORegalo() throws Exception {
+        PdfReader originale = new PdfReader(SRCVOR);
+        PdfStamper copiaModificata = new PdfStamper(originale, new FileOutputStream(DEST));
+        AcroFields placeholder = copiaModificata.getAcroFields();
+
+        placeholder.setField("ID", this.id);
+        placeholder.setField("EURO", this.importo);
+        placeholder.setField("MESSAGGIO", this.messaggio);
+
+        copiaModificata.setFormFlattening(true);
+        copiaModificata.close();
+        originale.close();
+    }
+}
+
